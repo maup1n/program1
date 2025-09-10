14d3165adc0891d0534607ed80baa86cfaa45fe5
mauricio <mauriciopintos361@gmail.com>
2025-09-10 16:13:14 -0300

    Actualizo samaqui1.1.py con nuevas líneas

--- samaqui1.1.py
+++ samaqui1.1.py
@@ -3,3 +3,15 @@ pal=input("ingrese una palabra por favor: ")
 print(f"la palabra en minuscula: {pal.lower()}")
 print(f"la palabra en mayuscula: {pal.upper()}")
 print(f"la palabra con la primera letra mayuscula:{pal.title() }")
+
+#parte 1.2
+frase = input("Ingresá una frase: ")
+palabras = frase.split()
+cantidad = len(palabras)
+print(f"La frase tiene {cantidad} palabras.")
+
+#parte 1.3
+palabra = input("Ingresá una palabra: ")
+for vocal in "aeiouAEIOU":
+    palabra = palabra.replace(vocal, "*")
+print("La palabra con vocales reemplazadas es:", palabra)
